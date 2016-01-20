b0bf5c55781309fb0ddc45bedc7aa07deb7b9963
lighter <unknownlighter@gmail.com>
2016-01-20 15:24:11 +0300

    packaging: add description

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name='sphericalmercator',
     version='0.0.2',
     py_modules = ['sphericalmercator',],
+    description = 'Tool for calculate XYZ-tile bounding box in spherical mercator',
     long_description=open('README.rst').read(),
     url='https://bitbucket.org/lighter/spherical-mercator/',
 )
