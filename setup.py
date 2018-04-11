aa676629898fc5d072c53735d836532a62c86e09
lighter <unknownlighter@gmail.com>
2018-04-12 00:14:09 +0300

    bump version to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sphericalmercator',
-    version='0.0.2',
+    version='0.1.0',
     py_modules = ['sphericalmercator',],
     description = 'Tool for calculate XYZ-tile bounding box in spherical mercator',
     long_description=open('README.rst').read(),
